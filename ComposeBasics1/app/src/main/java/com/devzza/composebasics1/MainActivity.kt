b91005564da2986e7245b2c7301763e2aaba343a
Mahmoud Ramzy <mahmoudramzy400@gmail.com>
2025-01-22 21:51:31 +0200

    Add finishing touches to codelab1

--- ComposeBasics1/app/src/main/java/com/devzza/composebasics1/MainActivity.kt
+++ ComposeBasics1/app/src/main/java/com/devzza/composebasics1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -15,8 +16,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
@@ -66,79 +75,6 @@ fun MyApp(
 
 }
 
-@Preview
-@Composable
-fun MyAppPreview() {
-    ComposeBasics1Theme {
-        MyApp(Modifier.fillMaxSize())
-    }
-}
-
-@Composable
-fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
-) {
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(
-            modifier = modifier
-                .padding(24.dp)
-        ) {
-            Column(
-                modifier = modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-
-            ) {
-                Text(text = "Hello")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)    )
-            }
-
-            ElevatedButton(
-                onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show Less" else "Show More")
-            }
-
-        }
-
-
-    }
-
-}
-
-@Preview(showBackground = true, widthDp = 320, uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "GreetingsPreviewDark" )
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GreetingsPreview() {
-    ComposeBasics1Theme {
-        Greetings()
-    }
-}
-
 @Composable
 fun OnBoardingScreen(
     onContinueClicked: () -> Unit,
@@ -156,11 +92,101 @@ fun OnBoardingScreen(
             modifier = Modifier.padding(vertical = 24.dp),
             onClick = onContinueClicked
         ) {
-            Text(text = "Continue")
+            Text(text = stringResource(R.string.action_continue))
         }
     }
 
 }
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+
+        CardContent(name)
+    }
+}
+
+
+@Composable
+private fun CardContent(name: String) {
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+
+        ) {
+            Text(text = "Hello")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4)
+                )
+            }
+        }
+
+        IconButton(onClick = { expanded = !expanded }) {
+
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(id = R.string.action_show_less)
+                } else {
+                    stringResource(id = R.string.action_show_more)
+                }
+            )
+
+        }
+
+
+    }
+}
+
+@Preview(
+    showBackground = true, widthDp = 320, uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "GreetingsPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    ComposeBasics1Theme {
+        Greetings()
+    }
+}
+
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
@@ -171,3 +197,17 @@ fun OnBoardingPreview() {
     }
 
 }
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeBasics1Theme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+
+
+
+
