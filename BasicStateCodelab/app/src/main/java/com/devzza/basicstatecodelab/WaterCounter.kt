9024d431f875faba89023d7e74a71dc2d5078e33
Mahmoud Ramzy <mahmoudramzy400@gmail.com>
2025-01-25 15:51:03 +0200

    Add the third codelab BasicStateCodelab

---
+++ BasicStateCodelab/app/src/main/java/com/devzza/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,46 @@
+package com.devzza.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * Created by: --- Mahmoud Ramzy
+ * Email: -------- mahmoudramzy400@gmail.com
+ * Date: --------- 25/01/2025
+ */
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text(
+                text = "You've had $count glasses.",
+            )
+        }
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(8.dp),
+            enabled = count > 10
+        ) {
+            Text("Add One")
+        }
+
+    }
+
+}
---
+++ BasicStateCodelab/app/src/main/java/com/devzza/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,17 @@
+package com.devzza.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+/**
+ * Created by: --- Mahmoud Ramzy
+ * Email: -------- mahmoudramzy400@gmail.com
+ * Date: --------- 25/01/2025
+ */
+
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
